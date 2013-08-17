d1ce41188494c40e1655754510aec005cd9acd66
Yangat <yangat@double-eye.com>
2013-08-17 21:40:09 +0200

    repairs

---
+++ java/com/htm/biz/RepairsRequisitionManager.java
@@ -0,0 +1,7 @@
+package com.htm.biz;
+
+/**
+ * Created by yanga on 2013/08/17.
+ */
+public class RepairsRequisitionManager {
+}
---
+++ java/com/htm/dto/Repair.java
@@ -0,0 +1,7 @@
+package com.htm.dto;
+
+/**
+ * Created by yanga on 2013/08/17.
+ */
+public class Repair {
+}
